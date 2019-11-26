889928429859f34b31f281dd78610949e6ec518c
HamzaMasood1 <hamzamasood183@gmail.com>
2019-11-26 05:27:47 +0000

    added consumes method

--- src/main/java/sample/SampleService.java
+++ src/main/java/sample/SampleService.java
@@ -7,7 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -95,6 +97,15 @@ public class SampleService {
 	@Produces("text/plain")
 	public String user(@PathParam("user")String user) {
 		return "Thanks "+ user;
+	}
+	
+	@POST
+	@Path("employees/change")
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	public Employee addEmployee(Employee employee) {
+		employees.put(employee.getEmployeeId(), employee);
+		return employee;
 	}
 	
 
